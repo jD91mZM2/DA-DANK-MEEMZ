1986412f5de20597f0225ef3f4cd512cf5c63b0d
LEGOlord208 <LEGOlord208@gmail.com>
2017-01-29 14:04:57 +0100

    AWW DERE AR ERRORZ HEREE NOW

--- main.go
+++ main.go
@@ -73,7 +73,11 @@ func main(){
 	load("weed", &Weed);
 	load("xfiles", &XFiles);
 
-	d, _ := discordgo.New("Bot " + token);
+	d, err := discordgo.New("Bot " + token);
+	if(err != nil){
+		fmt.Fprintln(os.Stderr, "I THINK THERE WAS ERROR ", err);
+		return;
+	}
 	d.AddHandler(ready);
 	d.AddHandler(messageCreate);
 	d.Open();
@@ -83,13 +87,19 @@ func main(){
 
 func load(file string, buffer *[][]byte){
 	f, err := os.Open("Dank/" + file + ".dca");
-	if err != nil { fmt.Println("File not found: " + file); return; }
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "FILE WAS WEIRD IDK: ", err);
+		return;
+	}
 
 	var length int16;
 	for {
 		err := binary.Read(f, binary.LittleEndian, &length);
 
 		if(err == io.EOF || err == io.ErrUnexpectedEOF){
+			return;
+		} else if(err != nil) {
+			fmt.Fprintln(os.Stderr, "IDK WAT U DID BUT WEL DONN NOOB, ", err);
 			return;
 		}
 
@@ -101,18 +111,36 @@ func load(file string, buffer *[][]byte){
 }
 
 func play(buffer [][]byte, session *discordgo.Session, guild, channel string, s *Settings){
+	var err error;
+	s.vc, err = session.ChannelVoiceJoin(guild, channel, false, true);
+	if(err != nil){
+		fmt.Fprintln(os.Stderr, "LEL FREGGIN NOOB, ", err);
+		return;
+	}
 	s.playing = true;
-	s.vc, _ = session.ChannelVoiceJoin(guild, channel, false, true);
 
-	s.vc.Speaking(true);
+	err = s.vc.Speaking(true);
+	if(err != nil){
+		fmt.Fprintln(os.Stderr, "I HAS TO TYPE ALL DESE ERROR MESSAGES " +
+		"HAVE SOME SYMPATHY PLS ", err);
+		return;
+	}
 
 	for _, buf := range buffer {
 		if s.vc == nil { return; }
 		s.vc.OpusSend <- buf;
 	}
 
-	s.vc.Speaking(false);
-	s.vc.Disconnect();
+	err = s.vc.Speaking(false);
+	if(err != nil){
+		fmt.Fprintln(os.Stderr, err);
+		return;
+	}
+	err = s.vc.Disconnect();
+	if(err != nil){
+		fmt.Fprintln(os.Stderr, err);
+		return;
+	}
 	s.playing = false;
 }
 
@@ -120,16 +148,18 @@ func messageCreate(session *discordgo.Session, event *discordgo.MessageCreate){
 	msg := strings.ToLower(strings.TrimSpace(event.Content));
 	author := event.Author;
 
-	channel, _ := session.State.Channel(event.ChannelID);
-	guild, _ := session.State.Guild(channel.GuildID);
-	//member, _ := session.State.Member(guild.ID, author.ID);
+	channel, err := session.State.Channel(event.ChannelID);
+	if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
+
+	guild, err := session.State.Guild(channel.GuildID);
+	if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
+	//member, err := session.State.Member(guild.ID, author.ID);
 
 	s := settings[guild.ID];
 	if(s == nil){
 		s = &Settings{};
 		settings[guild.ID] = s;
 	}
-	fmt.Println(*s);
 
 	if(s.commander != "" && s.commander != author.ID){
 		return;
@@ -137,44 +167,31 @@ func messageCreate(session *discordgo.Session, event *discordgo.MessageCreate){
 
 	var buffer [][]byte = nil;
 	switch(msg){
-		case "john cena":
-			buffer = JohnCena;
-		case "waiting":
-			buffer = Elevator;
-		case "rickroll":
-			buffer = Rickroll;
-		case "cri":
-			buffer = Cri;
-		case "letter":
-			buffer = Letter;
-		case "numbr hat":
-			buffer = NumberHat;
-		case "exotic butters":
-			buffer = ExoticButters;
-		case "damn son":
-			buffer = DamnSon;
-		case "jeff":
-			buffer = Jeff;
-		case "nigga":
-			buffer = Nigga;
-		case "russian singer":
-			buffer = RussianSinger;
-		case "sad violin":
-			buffer = SadViolin;
-		case "shut up":
-			buffer = ShutUp;
-		case "triple":
-			buffer = Triple;
-		case "turtur":
-			buffer = TurTur;
-		case "weed":
-			buffer = Weed;
-		case "illuminati":
-			buffer = XFiles;
+		case "john cena":		buffer = JohnCena;
+		case "waiting":			buffer = Elevator;
+		case "rickroll":		buffer = Rickroll;
+		case "cri":				buffer = Cri;
+		case "letter":			buffer = Letter;
+		case "numbr hat":		buffer = NumberHat;
+		case "exotic butters":	buffer = ExoticButters;
+		case "damn son":		buffer = DamnSon;
+		case "jeff":			buffer = Jeff;
+		case "nigga":			buffer = Nigga;
+		case "russian singer":	buffer = RussianSinger;
+		case "sad violin":		buffer = SadViolin;
+		case "shut up":			buffer = ShutUp;
+		case "triple":			buffer = Triple;
+		case "turtur":			buffer = TurTur;
+		case "weed":			buffer = Weed;
+		case "illuminati":		buffer = XFiles;
 		case "thx":
 			if(s.vc != nil){
-				s.vc.Speaking(false);
-				s.vc.Disconnect();
+				err := s.vc.Speaking(false);
+				if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
+
+				err = s.vc.Disconnect();
+				if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
+
 				s.playing = false;
 			}
 		case "listen only to me plz":
@@ -182,12 +199,15 @@ func messageCreate(session *discordgo.Session, event *discordgo.MessageCreate){
 		case "every1 owns u stopad robot":
 			s.commander = "";
 		case "cler da chat plz":
-			messages, _ := session.ChannelMessages(event.ChannelID, 100, "", "");
+			messages, err := session.ChannelMessages(event.ChannelID, 100, "", "");
+			if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
+
 			ids := make([]string, 0);
 			for _, message := range messages{
 				ids = append(ids, message.ID);
 			}
-			session.ChannelMessagesBulkDelete(event.ChannelID, ids);
+			err = session.ChannelMessagesBulkDelete(event.ChannelID, ids);
+			if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
 	}
 
 	if(buffer != nil && !s.playing){
@@ -203,8 +223,9 @@ func ready(session *discordgo.Session, event *discordgo.Ready){
 	ticker := time.NewTicker(time.Second * 5);
 	go func(){
 		for{
-			<- ticker.C;
-			session.UpdateStatus(0, statuses[rand.Intn(len(statuses))]);
+			<-ticker.C;
+			err := session.UpdateStatus(0, statuses[rand.Intn(len(statuses))]);
+			if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
 		}
 	}();
 }
