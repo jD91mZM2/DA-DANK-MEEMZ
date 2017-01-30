2bc25cb7ac024e35f8ea900b405ea06ad4760fdc
LEGOlord208 <LEGOlord208@gmail.com>
2017-01-30 09:43:34 +0100

    DIS IS EASIER KTHX

--- main.go
+++ main.go
@@ -1,232 +1,197 @@
-package main
+package main;
 
 import (
-	"os"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
-	"encoding/binary"
-	"io"
+	"os"
+	"encoding/json"
+	"io/ioutil"
 	"strings"
+	"regexp"
+	"strconv"
 	"time"
-	"math/rand"
 )
 
-var JohnCena = make([][]byte, 0);
-var Elevator = make([][]byte, 0);
-var Rickroll = make([][]byte, 0);
-var Letter = make([][]byte, 0);
-var Cri = make([][]byte, 0);
-var NumberHat = make([][]byte, 0);
-var ExoticButters = make([][]byte, 0);
-var DamnSon = make([][]byte, 0);
-var Jeff = make([][]byte, 0);
-var Nigga = make([][]byte, 0);
-var RussianSinger = make([][]byte, 0);
-var SadViolin = make([][]byte, 0);
-var ShutUp = make([][]byte, 0);
-var Triple = make([][]byte, 0);
-var TurTur = make([][]byte, 0);
-var Weed = make([][]byte, 0);
-var XFiles = make([][]byte, 0);
+var users map[string]int;
 
-var statuses = []string{
-	"hidden object games",
-	"Oh... Sir!",
-	"Minecraft 1.0 ALPHA",
-	"with your mother",
-	"something",
-	"something else",
-	"bored",
-	"dead"}
-
-type Settings struct{
-	vc *discordgo.VoiceConnection
-	playing bool
-	commander string
-}
-var settings = make(map[string]*Settings);
+var rNumber = regexp.MustCompile("-?[0-9]+");
+var rMentions = regexp.MustCompile("@[!0-9]+");
 
 func main(){
-	args := os.Args[1:];
-
-	if(len(args) < 1){
-		fmt.Println("No token provided!");
-		return;
-	}
-	token := args[0];
-
-	load("John Cena", &JohnCena);
-	load("Elevator", &Elevator);
-	load("Rickroll", &Rickroll);
-	load("Cri", &Cri);
-	load("Letter", &Letter);
-	load("NumberHat", &NumberHat);
-	load("ExoticButters", &ExoticButters);
-	load("damnson", &DamnSon);
-	load("jeff", &Jeff);
-	load("nigga", &Nigga);
-	load("russianSinger", &RussianSinger);
-	load("sadviolin", &SadViolin);
-	load("shutup", &ShutUp);
-	load("triple", &Triple);
-	load("turtur", &TurTur);
-	load("weed", &Weed);
-	load("xfiles", &XFiles);
-
-	d, err := discordgo.New("Bot " + token);
+	bytes, err := ioutil.ReadFile("timezones.json");
 	if(err != nil){
-		fmt.Fprintln(os.Stderr, "I THINK THERE WAS ERROR ", err);
+		fmt.Println("Note: timezones.json not read: ", err);
+		users = make(map[string]int, 0);
+	} else {
+		err = json.Unmarshal(bytes, &users);
+		if(err != nil){
+			fmt.Println("Couldn't parse JSON");
+			return;
+		}
+	}
+
+	args := os.Args[1:];
+	if(len(args) < 1){
+		fmt.Fprintln(os.Stderr, "No token specified!");
 		return;
 	}
-	d.AddHandler(ready);
-	d.AddHandler(messageCreate);
-	d.Open();
+	fmt.Println("Starting...");
+	discord, err := discordgo.New("Bot " + args[0]);
+	if(err != nil){
+		fmt.Fprintln(os.Stderr, "Couldn't start Discord bot");
+		return;
+	}
+
+	discord.AddHandler(message);
+	discord.Open();
 
 	<-make(chan struct{});
 }
 
-func load(file string, buffer *[][]byte){
-	f, err := os.Open("Dank/" + file + ".dca");
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "FILE WAS WEIRD IDK: ", err);
-		return;
+func save() error{
+	bytes, err := json.Marshal(users);
+	if(err != nil){
+		fmt.Fprintln(os.Stderr, "Could not generate JSON: ", err);
+		return err;
 	}
+	err = ioutil.WriteFile("timezones.json", bytes, 0666);
+	if(err != nil){
+		fmt.Fprintln(os.Stderr, "Could not save timezones.json: ", err);
+		return err;
+	}
+	return nil;
+}
 
-	var length int16;
-	for {
-		err := binary.Read(f, binary.LittleEndian, &length);
+func message(session *discordgo.Session, msg *discordgo.MessageCreate){
+	if(msg.Author.Bot){ return; }
 
-		if(err == io.EOF || err == io.ErrUnexpectedEOF){
+	content := strings.ToLower(strings.TrimSpace(msg.Content));
+	content = rMentions.ReplaceAllString(content, "");
+	if(strings.Contains(content, "my timezone")){
+		number := rNumber.FindString(content);
+		if(number == ""){
+			_, err := session.ChannelMessageSend(msg.ChannelID, "If you meant " +
+			"to change your timezone, please supply a number. Otherwise, " +
+			"sorry for disturbing!");
+			if(err != nil){
+				fmt.Fprintln(os.Stderr, "Could not send: ", err);
+				return;
+			}
 			return;
-		} else if(err != nil) {
-			fmt.Fprintln(os.Stderr, "IDK WAT U DID BUT WEL DONN NOOB, ", err);
+		}
+
+		n, err := strconv.Atoi(number);
+		if(err != nil){
+			fmt.Fprintln(os.Stderr, "String -> Int error: ", err);
 			return;
 		}
 
-		buf := make([]byte, length);
-		binary.Read(f, binary.LittleEndian, &buf);
+		users[msg.Author.ID] = n;
+		if(save() != nil){
+			return;
+		}
+		_, err = session.ChannelMessageSend(msg.ChannelID, "Set timezone for " +
+		msg.Author.Username + " to UTC+" + number);
 
-		*buffer = append(*buffer, buf);
-	}
-}
+		if(err != nil){
+			fmt.Fprintln(os.Stderr, "Could not send: ", err);
+			return;
+		}
+	} else if(strings.Contains(content, "time")){
+		number := rNumber.FindString(content);
+		if(number == ""){
+			for _, user := range msg.Mentions{
+				var err error;
+				if timezone, ok := users[user.ID]; ok{
+					t := time.Now().UTC().Add(time.Duration(timezone) * time.Hour);
+					format := t.Format("03:04:05 PM");
 
-func play(buffer [][]byte, session *discordgo.Session, guild, channel string, s *Settings){
-	s.playing = true;
-	var err error;
-	s.vc, err = session.ChannelVoiceJoin(guild, channel, false, true);
-	if(err != nil){
-		fmt.Fprintln(os.Stderr, "LEL FREGGIN NOOB, ", err);
-		s.playing = false;
-		return;
-	}
+					_, err = session.ChannelMessageSend(msg.ChannelID, "Current " +
+					"time for " + user.Username + " is " + format);
+				} else {
+					_, err = session.ChannelMessageSend(msg.ChannelID, "No " +
+					"timezone defined for " + user.Username + "!");
+				}
 
-	err = s.vc.Speaking(true);
-	if(err != nil){
-		fmt.Fprintln(os.Stderr, "I HAS TO TYPE ALL DESE ERROR MESSAGES " +
-		"HAVE SOME SYMPATHY PLS ", err);
-		return;
-	}
-
-	for _, buf := range buffer {
-		if s.vc == nil { return; }
-		s.vc.OpusSend <- buf;
-	}
-
-	err = s.vc.Speaking(false);
-	if(err != nil){
-		fmt.Fprintln(os.Stderr, err);
-		return;
-	}
-	err = s.vc.Disconnect();
-	if(err != nil){
-		fmt.Fprintln(os.Stderr, err);
-		return;
-	}
-	s.playing = false;
-}
-
-func messageCreate(session *discordgo.Session, event *discordgo.MessageCreate){
-	msg := strings.ToLower(strings.TrimSpace(event.Content));
-	author := event.Author;
-
-	channel, err := session.State.Channel(event.ChannelID);
-	if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
-
-	guild, err := session.State.Guild(channel.GuildID);
-	if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
-	//member, err := session.State.Member(guild.ID, author.ID);
-
-	s := settings[guild.ID];
-	if(s == nil){
-		s = &Settings{};
-		settings[guild.ID] = s;
-	}
-
-	if(s.commander != "" && s.commander != author.ID){
-		return;
-	}
-
-	var buffer [][]byte = nil;
-	switch(msg){
-		case "john cena":		buffer = JohnCena;
-		case "waiting":			buffer = Elevator;
-		case "rickroll":		buffer = Rickroll;
-		case "cri":				buffer = Cri;
-		case "letter":			buffer = Letter;
-		case "numbr hat":		buffer = NumberHat;
-		case "exotic butters":	buffer = ExoticButters;
-		case "damn son":		buffer = DamnSon;
-		case "jeff":			buffer = Jeff;
-		case "nigga":			buffer = Nigga;
-		case "russian singer":	buffer = RussianSinger;
-		case "sad violin":		buffer = SadViolin;
-		case "shut up":			buffer = ShutUp;
-		case "triple":			buffer = Triple;
-		case "turtur":			buffer = TurTur;
-		case "weed":			buffer = Weed;
-		case "illuminati":		buffer = XFiles;
-		case "thx":
-			if(s.vc != nil){
-				err := s.vc.Speaking(false);
-				if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
-
-				err = s.vc.Disconnect();
-				if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
-
-				s.playing = false;
+				if(err != nil){
+					fmt.Fprintln(os.Stderr, "Could not send: ", err);
+					return;
+				}
 			}
-		case "listen only to me plz":
-			s.commander = author.ID;
-		case "every1 owns u stopad robot":
-			s.commander = "";
-		case "cler da chat plz":
-			messages, err := session.ChannelMessages(event.ChannelID, 100, "", "");
-			if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
-
-			ids := make([]string, 0);
-			for _, message := range messages{
-				ids = append(ids, message.ID);
+		} else {
+			n, err := strconv.Atoi(number);
+			if(err != nil){
+				fmt.Fprintln(os.Stderr, "String -> Int error: ", err);
+				return;
 			}
-			err = session.ChannelMessagesBulkDelete(event.ChannelID, ids);
-			if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
-	}
 
-	if(buffer != nil && !s.playing){
-		for _, state := range guild.VoiceStates{
-			if state.UserID == event.Author.ID{
-				play(buffer, session, guild.ID, state.ChannelID, s);
+			is_12_h := n <= 12;
+			pm := (strings.Contains(content, "pm") || n == 12) && is_12_h;
+
+			n2 := n;
+			if(pm && n2 != 12){
+				n2 += 12;
+			} else if(!pm && n2 == 12){
+				n2 = 0;
+			}
+
+			if(n2 < 0 || n2 >= 24){
+				_, err = session.ChannelMessageSend(msg.ChannelID, "Invalid " +
+				"time. Has to be either 0-12am/pm or 0-23.");
+
+				if(err != nil){
+					fmt.Fprintln(os.Stderr, "Could not send: ", err);
+					return;
+				}
+				return;
+			}
+
+			timezone, ok := users[msg.Author.ID];
+			if(!ok){
+				_, err = session.ChannelMessageSend(msg.ChannelID, "Your " +
+				"timezone isn't set.");
+
+				if(err != nil){
+					fmt.Fprintln(os.Stderr, "Could not send: ", err);
+					return;
+				}
+				return;
+			}
+
+			for _, user := range msg.Mentions{
+				timezone2, ok := users[user.ID];
+				if(!ok){
+					_, err = session.ChannelMessageSend(msg.ChannelID,
+					user.Username + "'s timezone isn't set.");
+				} else {
+					t := n2 - timezone + timezone2;
+
+					pmstr := "";
+					if(pm){
+						pmstr = "PM ";
+					} else if(is_12_h){
+						pmstr = "AM ";
+					}
+
+					pmstr2 := "AM";
+					if(t >= 12){
+						if(t != 12){
+							t -= 12;
+						}
+						pmstr2 = "PM";
+					}
+
+					_, err = session.ChannelMessageSend(msg.ChannelID,
+					strconv.Itoa(n) + " " + pmstr + "your time is " +
+					strconv.Itoa(t) + " " + pmstr2 +
+					" for " + user.Username + ".");
+				}
+
+				if(err != nil){
+					fmt.Fprintln(os.Stderr, "Could not send: ", err);
+					return;
+				}
 			}
 		}
 	}
-}
-
-func ready(session *discordgo.Session, event *discordgo.Ready){
-	ticker := time.NewTicker(time.Second * 5);
-	go func(){
-		for{
-			<-ticker.C;
-			err := session.UpdateStatus(0, statuses[rand.Intn(len(statuses))]);
-			if(err != nil){ fmt.Fprintln(os.Stderr, err); return; }
-		}
-	}();
 }
